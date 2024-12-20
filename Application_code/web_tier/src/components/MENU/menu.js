20d804509242c569411a53b0ec166a3eafa69306
i-am-monika <mounikagarjakunta@gmail.com>
2024-12-20 16:58:36 +0530

    commit menu

---
+++ Application_code/web_tier/src/components/MENU/menu.js
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import { bool } from 'prop-types';
+import { StyledMenu } from './Menu.styled';
+import {
+  Link
+} from "react-router-dom";
+
+
+const Menu = ({ open, ...props }) => {
+  
+  const isHidden = open ? true : false;
+  const tabIndex = isHidden ? 0 : -1;
+
+  return (
+    <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" tabIndex = {tabIndex} style = {{outline:"none",border:"none"}}><div style={{paddingBottom : "2em", float:"left"}}><span aria-hidden="true">🏠</span> Home</div></Link>
+            </li>
+            <li>
+              <Link to="/db" tabIndex = {tabIndex} style = {{outline:"none",border:"none"}}><div style={{paddingBottom : "2em", float:"left"}}><span aria-hidden="true">📋</span> DB Demo</div></Link>
+OBOBOBOBOB            </li>
+          </ul>
+OB        </nav>
+OB      </div>
+    </StyledMenu>
+  );
+OB}
+
+OBMenu.propTypes = {
+  open: bool.isRequired,
+}
+
+export default Menu;
+
